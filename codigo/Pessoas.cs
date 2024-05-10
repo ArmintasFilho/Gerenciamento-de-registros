a6fee77ead0ac6f0ee6edbf060795bfecef32c55
ArmintasFilho <armintasfilho@gmail.com>
2024-05-10 19:10:26 -0300

    Criação da classe Pessoas.cs

---
+++ codigo/Pessoas.cs
@@ -0,0 +1,23 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Gerenciamento_de_registros
+{
+    public class Pessoas
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }  
+        public string Nome { get; set; }
+        public string Email { get; set; }
+        public DateTime Data_Nasc { get; set; }
+        public string Telefone { get; set; }
+
+    }
+}
