6c154a762c39bd527dd71d77c5e10fa80405bb42
ArmintasFilho <armintasfilho@gmail.com>
2024-05-10 19:10:48 -0300

    Criação do enum Operacao.cs

---
+++ codigo/Operacao.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gerenciamento_de_registros
+{
+    public enum Operacao
+    {
+        CONSULTAR,
+        ALTERAR,
+        ADICIONAR,
+        EXCLUIR
+    }
+}
